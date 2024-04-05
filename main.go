a44e1017ef0ab1b1080ef73b2be10dedb98e1274
Sean Whitaker <47946594+swhitake81@users.noreply.github.com>
2024-04-05 10:34:05 -0500

    added extra junk

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
-func Hello() {
-	log.Println("Hello, World!")
+func HelloRepeat() {
+	interval := 5
+	ticker := time.NewTicker(time.Second * time.Duration(interval))
+	for {
+		<-ticker.C
+		log.Println("Hello, World!")
+	}
+
 }
 
 func main() {
-	Hello()
+	HelloRepeat()
 }
